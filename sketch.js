01661d2d03c29000932e5b1db9e8c49e984c8078
Piyush258 <66780002+Piyush258@users.noreply.github.com>
2020-10-11 10:57:38 +0530

    My first multi webpage trial through realtime database by firebase consoloe

---
+++ sketch.js
@@ -0,0 +1,58 @@
+//Create variables here
+var d, happyDog, database, foodS, foodStock;
+var Dog;
+
+function preload()
+{
+  //load images here
+  d = loadImage("dogImg.png");
+  happyDog = loadImage("dogImg1.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+  Dog = createSprite(250,250,10,10);
+  Dog.addImage("dog",d);
+  Dog.scale = 0.25;
+
+  database = firebase.database();
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
+
+  
+}
+
+
+function draw() {  
+  background(46, 139, 87);
+  if (keyWentDown(UP_ARROW)){
+    writeStock(foodS);  
+  }
+  if (keyWentUp(UP_ARROW)){
+    Dog.addImage("dog", happyDog);
+  }
+  drawSprites();
+  //add styles here
+  textSize(20);
+  fill("blue");
+  stroke("black");
+  strokeWeight(1);
+  text("Press UP_ARROW to feed your pet!",90,125);
+  text("Food remaining :"+foodS,90,100);
+}
+function readStock(data){
+  foodS = data.val();
+}
+function writeStock(x){
+  if (x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+  database.ref('/').update({
+    Food:x
+  })
+}
+
+
+
